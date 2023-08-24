5fe05727b26f8138c70de346ca50614322b36368
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-24 22:16:53 +0300

    layout for converter completed

--- main.py
+++ main.py
@@ -19,14 +19,38 @@ window.resizable(height= FALSE, width=FALSE)
 top = Frame(window, width=368, height= 60, bg = color3)
 top.grid(row = 0,column=0)
 
-main = Frame(window, width=368, height = 300, bg = color2 )
+main = Frame(window, width=368, height = 300, bg = color4 )
 main.grid(row =1, column=0)
 
 #top frame
 icon = Image.open('images/convertericon.png')
 icon = icon.resize((40, 40))
 icon = ImageTk.PhotoImage(icon)
-app_name = Label(top, image = icon, compound= LEFT, text ="Ugyen's Currency Converter", height = 5, padx=13, pady=30, anchor=CENTER, font=('Arial 16 bold'), bg=color4, fg=color5)
+app_name = Label(top, image = icon, compound= LEFT, text ="Ugyen's Currency Converter", height = 5, padx=13, pady=30,relief="solid", anchor=CENTER, font=('Arial 16 bold'), bg=color2, fg=color4)
 app_name.place(x=0,y=0)
 
+#main frame
+result = Label(main, text=" ",width=21, height=2, padx=4, pady=10, anchor=CENTER, relief="solid", font=('Ivy 15 bold'), bg=color4, fg=color3)
+result.place(x=50,y=15)
+
+from_label = Label(main,width=21, text ="From", height = 5, padx=0, pady=0,relief="flat", anchor=NW, font=('Ivy 12'), bg=color4, fg=color3)
+from_label.place(x=50, y=100)
+
+currency = ['Euro', 'AUD', 'USD', 'NRs']
+comboFrom = ttk.Combobox(main, width=10, justify=CENTER, font=("Ivy 12 "))
+comboFrom['values'] =(currency)
+comboFrom.place(x=50, y=125)
+
+to_label = Label(main,width=21, text ="To", height = 5, padx=0, pady=0,relief="flat", anchor=NW, font=('Ivy 12'), bg=color4, fg=color3)
+to_label.place(x=200, y=100)
+
+comboTo =ttk.Combobox(main, width=10, justify=CENTER, font=("Ivy 12"))
+comboTo['values'] = (currency)
+comboTo.place(x=200, y=125)
+
+value = Entry(main, width=29, justify=CENTER, font=("Ivy 12 bold"), relief="solid")
+value.place(x=50, y=170)
+
+button =Button(main, text="Convert", width=25, padx=5, height=1, bg=color2, fg=color4, font=("Ivy 12 bold"), justify=CENTER)
+button.place(x=50, y=210)
 window.mainloop()
